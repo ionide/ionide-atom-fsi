d66192dc9bf8113f9d46430f5184cae0349c9aa3
Krzysztof Cieślak <krzysztof_cieslak@windowslive.com>
2015-09-21 15:31:13 +0200

    Bump version to 1.0.4

--- src/fsi.fs
+++ src/fsi.fs
@@ -115,8 +115,8 @@ module FsiService =
         subscriptions.Add s
 
         if Process.isWin () |> not && fsipath = "C:/Program Files (x86)/Microsoft SDKs/F#/4.0/Framework/v4.0/Fsi.exe" then
-            Globals.atom.config.set("ionide-fsi.FsiPath", "fsharpi") 
-            fsipath <- "fsharpi"
+            Globals.atom.config.set("ionide-fsi.FsiPath", "fsharpi")
+            fsipath <- "fsharpi" 
 
 
 
